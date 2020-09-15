7f62c3dd1bece89d697045876c677f6bf39d5b4a
RicoRicoCA <rregister@gmail.com>
2020-09-14 20:21:18 -0700

    After newpage submit, check if get-entry is NONE

--- wiki/newpage/views.py
+++ wiki/newpage/views.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.shortcuts import render
 from django.http import HttpResponse
+from encyclopedia import util
+
 class NewTaskForm(forms.Form):
     title = forms.CharField(label="Title")
     entry = forms.CharField(widget=forms.Textarea(attrs={"style": "height:400px; resize: none;"}))
@@ -12,9 +14,12 @@ def index(request):
         form = NewTaskForm(request.POST)
         if form.is_valid():
             title = form.cleaned_data["title"]
-            entry = form.cleaned_data["entry"]
-#            return HttpResponseRedirect(reverse("tasks:index"))
-            return HttpResponse("Success:" + title + "<br>" + "<a href=" + "/" + ">Home</a>")
+            if util.get_entry(title) == None:
+                entry = form.cleaned_data["entry"]
+                #write entry to md file
+                return HttpResponse("Success:" + title + "<br>" + "<a href=" + "/" + ">Home</a>")
+            else:
+                return HttpResponse("Error:" + title + " already exists<br>" + "<a href=" + "/" + ">Home</a>")
         else:
             return render(request, "tasks/add.html", {
                 "form": form
