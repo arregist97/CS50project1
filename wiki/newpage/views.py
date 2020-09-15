a59a7693243575e006a1c5699486459cfe544a47
RicoRicoCA <rregister@gmail.com>
2020-09-14 19:40:54 -0700

    newpage has a title and entry and submit calls post

--- wiki/newpage/views.py
+++ wiki/newpage/views.py
@@ -1,6 +1,25 @@
+from django import forms
 from django.shortcuts import render
+from django.http import HttpResponse
+class NewTaskForm(forms.Form):
+    title = forms.CharField(label="Title")
+    entry = forms.CharField(widget=forms.Textarea(attrs={"style": "height:400px; resize: none;"}))
 
 
 # Create your views here.
 def index(request):
-    return render(request, "newpage/index.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            entry = form.cleaned_data["entry"]
+#            return HttpResponseRedirect(reverse("tasks:index"))
+            return HttpResponse("Success:" + title + "<br>" + entry)
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
+    return render(request, "newpage/index.html", {
+            "form": NewTaskForm()
+        })
