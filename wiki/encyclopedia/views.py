c2c2546d3a542f5a98df79a4ea57af84acc62b4d
Arthur <arregist@ucsc.edu>
2020-09-15 12:42:10 -0700

    entry template added, updated gitignore.

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from markdown2 import Markdown
 from django.http import HttpResponse
 
-
+#python manage.py runserver
 from . import util
 
 
@@ -13,9 +13,16 @@ def index(request):
 def css(request):
     markdowner = Markdown()
     csspage = util.get_entry("CSS")
+    body = markdowner.convert(csspage)
     #markdowner.convert(fileReader.read)
     #return render(request, "encyclopedia/index.html", {
     #    "entries": util.list_entries()
     #})
-    return HttpResponse(markdowner.convert(csspage))
+    #return HttpResponse('{% extends "encyclopedia/layout.html" %}{% block title %}' + body + '{% endblock %}')
+#    return render(request, "encyclopedia/entry.html", {
+#        "text": "hi"
+#    })'''
+    return render(request, "encyclopedia/entry.html", {
+        "text": body
+    })
 
