4e340e559c7571f1f46d53215a47a0a006d04442
RicoRicoCA <rregister@gmail.com>
2020-09-15 11:31:09 -0700

    Remove encyclopedia appname from urls.py

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = "encyclopedia"
+#app_name = "encyclopedia"
 urlpatterns = [
     path("CSS", views.css, name="CSS"),
     path("", views.index, name="index")
