70cef65c9af62514bf8710d13f4df64f9097c7d9
Arthur <arregist@ucsc.edu>
2020-09-19 12:50:14 -0700

    added search feature

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,6 +5,7 @@ from . import views
 #app_name = "encyclopedia"
 urlpatterns = [
     path("CSS", views.css, name="CSS"),
+    path("search/", views.search, name="query"),
     path("<str:entry>", views.page, name="entry"),
     path("", views.index, name="index")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 from . import util
 
 
+
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
@@ -34,4 +35,30 @@ def page(request, entry):
         "text": body, 
         "title": entry
     })
+def search(request):
+    if request.method == "POST":
+        ibcm = request.POST
+        query = ibcm['query']
+        #make everything lowercase for comparisons
+        entries = util.list_entries()
+        #find all matches
+        matches = []
+        for entry in entries:
+            if not((entry.lower()).find(query.lower()) == -1):
+                matches.append(entry)
+        #render matches
+        if(len(matches) == 0):
+            return HttpResponse("No matches found for " + query + ".<br>" + "<a href=" + "/" + ">Home</a>")
+        elif(len(matches) == 1):
+            markdowner = Markdown()
+            mdpage = util.get_entry(matches[0])
+            body = markdowner.convert(mdpage)
+            return render(request, "encyclopedia/entry.html", {
+                "text": body, 
+                "title": matches[0]
+        })
+        else:
+            return render(request, "encyclopedia/index.html", {
+                "entries": matches
+            })
 
